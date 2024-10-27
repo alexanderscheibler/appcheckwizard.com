68fa9c3d9ff1b67a4b4e3898d847dafd873d4964
Alexander Scheibler <60936422+alexanderscheibler@users.noreply.github.com>
2024-10-26 21:03:58 -0300

    feat: portfolio page

---
+++ app/components/Help.tsx
@@ -0,0 +1,58 @@
+import { ChevronDown } from "lucide-react";
+import Link from "next/link";
+
+export default function Help() {
+
+    const items = [
+        {
+            title: "Is my app always on?",
+            description: "Let's work together to configure monitoring and alerts, so critical issues are caught by your team and resolved before they affect your customers.",
+        },
+        {
+            title: "Is my app working?",
+            description: "How about we do some functional testing of the most important parts, to ensure they are doing what is supposed to be doing?",
+        },
+        {
+            title: "Will my app keep working if I change things?",
+            description: "We could set up automated tests that verify core functionality every time an update is made.",
+        },
+        {
+            title: "Can my app scale as my business grows?",
+            description: "Running performance tests will offer you some answers about how many users your app can safely handle.",
+        },
+        {
+            title: "Is my data safe and secure?",
+            description: "Analysis and security questionnaires tailored to your business needs, ensuring compliance with standards, including for LGPD in Brazil.",
+        },
+        {
+            title: "What happened with my app?!",
+            description: "With expertise in investigation and Root Cause Analysis (RCA), we can identify what happened, why it happened, and the steps to prevent it in the future.",
+        }
+    ]
+
+    return (
+    <section id="help" className="py-20 bg-gray-900">
+        <div className="container mx-auto px-6">
+            <h2 className="text-3xl font-semibold text-center mb-12">How can I help?</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {items.map((item, index) => (
+                    <div key={index}
+                         className="bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+                        <div className="p-6">
+                            <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
+                            <p className="text-gray-300 mb-4">{item.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="flex items-center justify-center">
+                <Link href="#projects" aria-label="Projects. What have I done" className="pt-14 inline-block animate-bounce">
+                    <ChevronDown size={32}/>
+                </Link>
+            </div>
+
+        </div>
+    </section>
+)
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_DOMAIN: process.env.NEXT_PUBLIC_SUPABASE_DOMAIN,
+    NEXT_PUBLIC_MOCK_ITEMS: process.env.NEXT_PUBLIC_MOCK_ITEMS,
+  },
+
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/appcheckwizard.com' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/appcheckwizard.com' : '',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
