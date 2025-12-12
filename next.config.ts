337d2e325dc94a985a91f4d9ab96f4acd06146d8
Alexander Scheibler <60936422+alexanderscheibler@users.noreply.github.com>
2025-12-12 00:46:17 -0400

    fix: adjust Next.js output to allow server-side functionality

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  output: 'export',
   images: {
     unoptimized: true,
   },
   async rewrites() {
     return [
-      {
-        source: '/js/script.js',
-        destination: 'https://plausible.io/js/plausible.js',
-      },
-      {
-        source: '/api/event',
-        destination: 'https://plausible.io/api/event',
-      },
     ]
   }
 };
